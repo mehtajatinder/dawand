f4b6f0014f773066ebc1adf8278e40207e85c0c8
nipun <nipunkuchroo2007@gmail.com>
2021-02-16 12:47:20 +0530

    changes

--- src/app/models/user-model.ts
+++ src/app/models/user-model.ts
@@ -1,19 +1,21 @@
 export class UserData {
-  firstName!: string;
-  lastName!: string;
-  password!: string;
-  confirmPassword?: string;
-  email!: string;
-  gender!: string;
-  fathersName?: string;
-  husbandsName?: string;
-  adhaar!: number;
-  phoneNumber!: number;
-  alternateNumber?: number;
-  designation!: string;
-  deptName!: string;
-  area!: string;
-  district!: string;
-  villageName!: string;
-  affilatedGurudwara!: string;
+  constructor(
+    private token: string,
+    public userID: string,
+    public firstName?: string,
+    public lastName?: string,
+    public email?: string,
+    public gender?: string,
+    public fathersName?: string,
+    public husbandsName?: string,
+    public adhaar?: number,
+    public phoneNumber?: number,
+    public alternateNumber?: number,
+    public designation?: string,
+    public deptName?: string,
+    public area?: string,
+    public district?: string,
+    public villageName?: string,
+    public affilatedGurudwara?: string
+  ) {}
 }
