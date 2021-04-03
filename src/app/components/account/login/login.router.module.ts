d91745c82c5575b53e158c667375ecd7a2b515f4
nipun kuchroo <nipunkuchroo2007@gmail.com>
2021-04-03 22:52:03 +0530

    chnages

--- src/app/components/account/login/login.router.module.ts
+++ src/app/components/account/login/login.router.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { authGaurd } from 'src/app/authGaurd/authGaurd.service';
 import { LoginComponent } from './login.component';
 
 const loginRoutes = [{ path: '', component: LoginComponent }];
--- src/app/router/router.module.ts
+++ src/app/router/router.module.ts
@@ -38,6 +38,7 @@ const routes: Routes = [
   },
   {
     path: 'payment',
+    canActivate: [authGaurd],
     component: paymentComponent,
     canDeactivate: [canDeactivateGaurd],
   },
